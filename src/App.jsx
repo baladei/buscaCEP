1c725a0b5a5af80a3f69778c6350d07e3409a588
Fernando Baladei <f.baladei@gmail.com>
2022-12-01 06:33:28 -0300

    finishing styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
-export function App() {
+import { FiSearch } from 'react-icons/fi'
+import './styles/styles.css'
+
+function App() {
   
   return (
-    <h1>Testando</h1>
-  )
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        
+        <input
+        type="text"
+        placeholder="Digite o CEP"
+        />
+
+        <button className='searchButton'>
+          <FiSearch size={25} color='#FFF' />
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 04349280</h2>
+
+        <span>Rua das Aningas</span>
+        <span>Complemento: não tem</span>
+        <span>Jardim Oriental</span>
+        <span>São Paulo - SP</span>
+
+      </main>
+
+    </div>
+  );
 }
+
+export default App
